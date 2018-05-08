5f6b630e376978882ed5a295d8f2645c0341ec46
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2018-05-08 16:11:41 -0500

    Check function defined

--- src/semantic.py
+++ src/semantic.py
@@ -6,15 +6,47 @@ from nltk.sem import Valuation, Model
 def semantic(paragraph):
     print(paragraph)
     functionDefine = {}
+    onString = False
+    nextTag = ""
+    previusTag = ""
     for sentence in paragraph:
         for value, tag in sentence:
-            if tag == "NNP":
-                # print value + " tag: " + tag
-                functionDefine[value] = +1
+            if tag == "STRSTART":
+                onString = True
+            if onString == False:
+                if nextTag == tag:
+                    functionDefine[previusTag] = 2
+                    print(functionDefine[previusTag])
+                    nextTag = ""
+                    previusTag= ""
+                else:
+                    nextTag = ""
+                    previusTag= ""
+
+                if tag == "NNP":
+                    if (value in functionDefine) == False:
+                        functionDefine[value] = 1
+                        nextTag = "ASSIGN"
+                        previusTag = value
+                    if (value in functionDefine) == True:
+                        functionDefine[value] = 2
+                        nextTag = "ASSIGN"
+                        previusTag = value
+            if onString == True:
+                if tag == "STRSTOP":
+                    onString = False
 
         for value, tag in sentence:
-            if tag == "NNP":
-                if functionDefine[value] <= 2:
-                    error = "Error: Function " + value + " is not defined"
-                    return error
+            if tag == "STRSTART":
+                onString = True
+            if onString == False:
+                if tag == "NNP":
+                    if functionDefine[value] < 2:
+                        error = "Error: Function " + value + " is not defined"
+                        print(functionDefine[value])
+                        return error
+            if onString == True:
+                if tag == "STRSTOP":
+                    onString = False
+
     return True
